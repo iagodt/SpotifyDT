2f947c8bb86052ad4270478aa504fa2ff90a0ec8
iagodt <iago.041704@gmail.com>
2022-11-17 03:43:12 -0300

    adding login w/ spotify
    Login in app with spotify by oauth

--- src/login/vue/login.js
+++ src/login/vue/login.js
@@ -1,14 +1,16 @@
 const login = {
-    data(){
+    data: () => {
         return {
-            count: 0
+            clientID: '0ddea5cff2e24789a34fa32ad94bd329',
+            scope: 'app-remote-control streaming user-read-currently-playing user-modify-playback-state user-read-playback-state playlist-read-private playlist-read-collaborative user-library-read user-follow-modify user-follow-read',
+            uri: 'http://localhost:25565/index.html',
         }
     },
     methods:{
         teste(){
-            console.log('teste - login');
+            location.href ='https://accounts.spotify.com/authorize?response_type=code&client_id=0ddea5cff2e24789a34fa32ad94bd329&scope=app-remote-control streaming user-read-currently-playing user-modify-playback-state user-read-playback-state playlist-read-private playlist-read-collaborative user-library-read user-follow-modify user-follow-read&redirect_uri=http://localhost:25565/index.html';
         }
     }
 }
 
-Vue.createApp(login).mount('#login');
+Vue.createApp(login).mount('.login');
