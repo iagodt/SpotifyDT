4b9d76d4a81117d6780bedcd0fccbfb9a257202a
iagodt <iago.041704@gmail.com>
2022-12-15 20:02:12 -0300

    Refac login screen
    Change vuejs to React JS

--- public/src/login/vue/login.js
+++ public/src/login/vue/login.js
@@ -1,17 +1,22 @@
-const login = {
-    data: () => {
-        return {
+class Login extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
             clientID: '0ddea5cff2e24789a34fa32ad94bd329',
             scope: 'app-remote-control streaming user-read-currently-playing user-modify-playback-state user-read-playback-state playlist-read-private playlist-read-collaborative user-library-read user-follow-modify user-follow-read',
-            uri: 'http://localhost:17280/home',
-        }
-    },
-    methods:{
-        login(){
-            location.href =`https://accounts.spotify.com/authorize?response_type=code&client_id=${this.clientID}&scope=${this.scope}&redirect_uri=${this.uri}`;
+            uri: 'http://localhost:17280/home'
+
         }
     }
+    render() {
+        return (
+            <div id='login'>
+                <img id='logo' src="src/login/img/SpotifyDT.svg"></img>
+                <button id='login_btn' onClick={()=>location.href = `https://accounts.spotify.com/authorize?response_type=code&client_id=${this.state.clientID}&scope=${this.state.scope}&redirect_uri=${this.state.uri}`}>LOGIN</button>
+            </div>
+            
+        )
+    }
 }
-
-
-Vue.createApp(login).mount('.login');
+const login = ReactDOM.createRoot(document.getElementById('root'))
+login.render(<Login />)
